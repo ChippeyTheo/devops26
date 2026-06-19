d41dc647bbaa0845969f597c72c61504a387b2b3
unknown <chippey.theo@gmail.com>
2026-06-19 18:16:14 +0200

    tp3

---
+++ tp-wik-dps-03/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		notFound(w)
+		return
+	}
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "unknown"
+	}
+
+	fmt.Printf("[PING] Requête reçue sur le conteneur : %s\n", hostname)
+
+	headers := make(map[string][]string)
+	for k, v := range r.Header {
+		headers[k] = v
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(headers)
+}
+
+func notFound(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusNotFound)
+}
+
+func main() {
+	port := os.Getenv("PING_LISTEN_PORT")
+	if port == "" {
+		port = "9090"
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", pingHandler)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	fmt.Printf("Server running at http://localhost:%s/ping\n", port)
+
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
